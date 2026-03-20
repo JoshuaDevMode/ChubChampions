052e54b10e4fd58b7a98e2e4bcbe2c4df2c185ea
JoshuaDevMode <JoshuaDevMode@users.noreply.github.com>
2026-03-20 00:23:12 +0000

    deploy: 2af34ba25127b5b34593f3e584505b4942cfdc89

--- assets/all-wallets-CDNI88kZ.js
+++
@@ -1,6 +0,0 @@
-import{w as l}from"./core-DWVW3-0W.js";import"./index-D1Ab-BBV.js";const t=l`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{t as allWalletsSvg};
